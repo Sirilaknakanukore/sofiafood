e1a5d76bb4f29550c1c60c1630c864b70ff168ff
sirilak <non-sofie@hotmail.com>
2019-05-21 14:49:56 +0700

    Create Title and Logo Success

---
+++ lib/screens/authen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Authen extends StatefulWidget {
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  Widget showlogo() {
+    return Image.asset('images/logo.png');
+  }
+
+  Widget showTitle() {
+    return Text(
+      'Sofia Food',
+      style: TextStyle(
+          fontSize: 25.0, fontWeight: FontWeight.bold, color: Colors.red[300]),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      margin: EdgeInsets.only(top: 70.0),
+      alignment: Alignment(0, -1),
+      child: Column(
+        children: <Widget>[
+          showlogo(),
+          Container(
+            margin: EdgeInsets.only(top: 20.0),
+            child: showTitle(),
+          )
+        ],
+      ),
+    ));
+  }
+}
